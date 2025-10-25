9dc110e017493eeeb98156a11754b28c591e3caa
Filippov-DC <danila124242@gmail.com>
2025-10-25 17:18:27 +0300

    Initial commit: Saucedemo test automation project

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.edge.EdgeDriverService;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    
+    @BeforeMethod
+    public void setUp() {
+        if (driver == null) {
+            System.setProperty("webdriver.edge.driver", "C:\\Users\\danya\\Desktop\\msedgedriver\\msedgedriver.exe");
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--start-maximized");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            EdgeDriverService service = new EdgeDriverService.Builder()
+                    .usingDriverExecutable(new File("C:\\Users\\danya\\Desktop\\msedgedriver\\msedgedriver.exe"))
+                    .build();
+            driver = new EdgeDriver(service, options);
+            driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+        }
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+    }
+    
+    @AfterSuite
+    public void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
